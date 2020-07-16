1ac11f52b39fe4580c0b9411ab201fa243730855
zhangyufei <zyf_china@aliyun.com>
2020-07-16 15:55:43 +0800

    项目初始化

---
+++ springboot/src/main/java/com/himoyi/salary_management_system/SalaryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.himoyi.salary_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ springboot/src/test/java/com/himoyi/salary_management_system/SalaryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himoyi.salary_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
